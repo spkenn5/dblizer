4cf4c9389c1a97597a1fb8da1042b76aad42a4bb
Kenji <spkenn5@gmail.com>
2019-03-16 10:51:50 +0800

    add mysql

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ class app {
     }
 
     loadServer() {
+        const mysql = require('mysql')
+        const options = {
+            user: 'dev',
+            password: 'maingames',
+            database: 'playground'
+          }
+        const connection = mysql.createConnection(options)
+        connection.connect(err => {
+            if (err) {
+              console.error('An error occurred while connecting to the DB')
+              throw err
+            }
+          });
+
+        connection.query('SELECT * FROM cards', (error, todos, fields) => {
+           if (error) {
+             console.error('An error occurred while executing the query')
+             throw error
+           }
+           console.log("mother shit ", todos, fields);
+        });
+
         const HTTP = require('http');
         const PORT = 8000;
 
