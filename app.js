c7f19727b855dd889d687d467bc1d4b442939e02
kenji-v <kenji-v@crimsonlogic.com>
2019-03-18 18:31:59 +0800

    resolve conflict

--- app.js
+++ app.js
@@ -6,36 +6,55 @@ class app {
     }
 
     loadServer() {
-        const mysql = require('mysql')
-        const options = {
-            user: 'dev',
-            password: 'maingames',
-            database: 'playground'
-          }
-        const connection = mysql.createConnection(options)
-        connection.connect(err => {
-            if (err) {
-              console.error('An error occurred while connecting to the DB')
-              throw err
-            }
-          });
+        const fs = require('fs');
+        const _ = require('lodash');
+        const mysql = require('mysql');
+        const config = require('./config/config.json');
 
-        connection.query('SELECT * FROM cards', (error, todos, fields) => {
-           if (error) {
-             console.error('An error occurred while executing the query')
-             throw error
-           }
-           console.log("mother shit ", todos, fields);
+        const defaultConfig = config.development;
+        const environment = process.env.NODE_ENV || 'development';
+        const environmentConfig = config[environment];
+        const finalConfig = _.merge(defaultConfig, environmentConfig);
+
+        const db = mysql.createConnection ({
+            host: finalConfig.db_host,
+            user: finalConfig.db_user,
+            password: finalConfig.db_password,
+            database: finalConfig.db_name
+        });
+
+        global.db = db;
+        global.app_config = finalConfig;
+
+        const testQ = "select * from cb_rfq order by id desc limit 5";
+        db.query(testQ, (err, result) => {
+            if(!err){
+                console.log('DEBUG result -> ', result.map(JSON.stringify));
+            }else{
+                console.log('DEBUG error -> ', err);
+            }
         });
 
         const HTTP = require('http');
+        // require the new module
+        var handler = require('./resources/js/handler');
+        var server = HTTP.createServer((request, response) => {
+            var parts = url.parse(request.url);
+            if (parts.pathname === '/') {
+                handler(request, response);
+            } else {
+                // error handle unknown path
+            }
+        });
         const PORT = 8000;
-
-        HTTP.createServer((request, response) => {
-            response.writeHead(200, {'Content-Type': 'text/plain'});
-            response.write(`Look at my website!`);
-            response.end();
-        }).listen(PORT);
+        fs.readFile('./resources/html/index.html', function (err, html) {
+            if (err) {
+                throw err;
+            }
+            server.listen(PORT, () => {
+                console.log('Server is now running at port ', PORT);
+            });
+        });
     }
 }
 
